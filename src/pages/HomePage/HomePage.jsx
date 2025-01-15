7f2c77424ed75167429246b8da9597d031394154
Nadiia Yuce <soyka9616@gmail.com>
2025-01-15 21:55:49 +0100

    add hero section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+import css from "./HomePage.module.css";
+
 export default function HomePage() {
-  return <div style={{ backgroundColor: "red" }}>Home</div>;
+  return (
+    <section className={css.hero}>
+      <h1 className={css.title}>Campers of your dreams</h1>
+      <p className={css.subtitle}>
+        You can find everything you want in our catalog
+      </p>
+      <Link to="/catalog" className={css.btn}>
+        View Now
+      </Link>
+    </section>
+  );
 }
