5e5f73f95bf6ea1eb6f52ca9294f0ca94931c847
Nadiia Yuce <soyka9616@gmail.com>
2025-01-15 21:05:16 +0100

    add header and  Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,26 @@
+import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
+import clsx from "clsx";
+import sprite from "../../icons/sprite.svg";
 
 export default function Navigation() {
+  const getActiveClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
+
   return (
-    <div className={css.header}>
-      <h1>Hello!</h1>
-    </div>
+    <header className={css.header}>
+      <svg className={css.logo}>
+        <use href={`${sprite}#icon-logo`} />
+      </svg>
+      <nav>
+        <NavLink className={getActiveClass} to="/">
+          Home
+        </NavLink>
+        <NavLink className={getActiveClass} to="/catalog">
+          Catalog
+        </NavLink>
+      </nav>
+    </header>
   );
 }
